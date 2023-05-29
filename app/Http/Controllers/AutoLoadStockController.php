a63b20a6ee167d26c63933b6d5efa0aa899d6644
MeghaThapa <meghathapaa@gmail.com>
2023-05-29 21:57:34 +0545

    autoloaditem stock done

--- app/Http/Controllers/AutoLoadStockController.php
+++ app/Http/Controllers/AutoLoadStockController.php
@@ -5,24 +5,53 @@ use App\Helpers\AppHelper;
 use Illuminate\Http\Request;
 use App\Models\AutoLoadItemStock;
 use App\Models\Department;
+use DB;
 class AutoLoadStockController extends Controller
 {
     public function index(){
          $helper= new AppHelper();
          $settings= $helper->getGeneralSettigns();
-         $autoloadStockDatas=null;
-         $autoloadStocks=AutoLoadItemStock::with('plantType','plantName','danaName','shift')->get();
-         $godams=Department::where('status','active')->get();
+          $autoloadStocks= DB::table('auto_load_item_stocks AS stock')
+          ->join('processing_steps AS plantType', 'stock.plant_type_id', '=', 'plantType.id')
+          ->join('processing_subcats AS plantName', 'stock.plant_name_id', '=', 'plantName.id')
+          ->join('dana_names AS danaName', 'stock.dana_name_id', '=', 'danaName.id')
+          ->join('shifts AS shift', 'stock.shift_id', '=', 'shift.id')
+          ->select(
+              'plantType.name as plantTypeName',
+              'plantName.name as plantName',
+              'danaName.name as danaName',
+              'shift.name as shiftName',
+              'stock.quantity',
+          )
+          ->paginate(35);
+
+        // $autoloadStocksData=AutoLoadItemStock::with('plantType','plantName','danaName','shift')->paginate(15);
+
+         $godams=Department::where('status','active')->get(['id','department']);
          //return $godams;
-         return view('admin.autoloadStock.index',compact('settings','autoloadStocks','godams','autoloadStockDatas'));
+         return view('admin.autoloadStock.index',compact('settings','autoloadStocks','godams'));
     }
     public function filterAccGodam(Request $request){
             $helper= new AppHelper();
             $settings= $helper->getGeneralSettigns();
-            $autoloadStockDatas=AutoLoadItemStock::where('from_godam_id',$request->godam_id)->get();
-            $autoloadStocks=null;
-            $godams=Department::where('status','active')->get();
+            $autoloadStocks= DB::table('auto_load_item_stocks AS stock')
+            ->when($request->godam_id != 'all', function ($query) use ($request) {
+                return $query->where('stock.from_godam_id', $request->godam_id);
+            })
+          ->join('processing_steps AS plantType', 'stock.plant_type_id', '=', 'plantType.id')
+          ->join('processing_subcats AS plantName', 'stock.plant_name_id', '=', 'plantName.id')
+          ->join('dana_names AS danaName', 'stock.dana_name_id', '=', 'danaName.id')
+          ->join('shifts AS shift', 'stock.shift_id', '=', 'shift.id')
+          ->select(
+              'plantType.name as plantTypeName',
+              'plantName.name as plantName',
+              'danaName.name as danaName',
+              'shift.name as shiftName',
+              'stock.quantity',
+          )
+          ->paginate(35);
+         $godams=Department::where('status','active')->get(['id','department']);
        // return $autoloadStockDatas;
-            return view('admin.autoloadStock.index',compact('settings','autoloadStockDatas','godams','autoloadStocks'));
+            return view('admin.autoloadStock.index',compact('settings','autoloadStocks','godams'));
     }
 }
--- resources/views/admin/autoloadStock/index.blade.php
+++ resources/views/admin/autoloadStock/index.blade.php
@@ -1,24 +1,33 @@
 @extends('layouts.admin')
 
+@section('extra-style')
+    <link href="{{ asset('css/select2/select2.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/select2/select2-bootstrap4.css') }}" rel="stylesheet" />
+@endsection
 @section('content')
+
+<div style="display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+    align-items: center;" >
+
+  <img class="lg-logo" src="{{ $settings->logo }}" alt="{{ $settings->companyName}}" width="250" height="50">
+  <h3>Auto Load Stock</h3>
+</div>
 <form action="{{route('autoloadStock.filterAccGodam')}}" method="POST">
     @csrf
- <div class="row">
-            <div style="display:flex;gap:10px;">
-            <label for="">Godam</label>
+ <div style="display:flex;gap:10px; width:400px;justify-content: center;align-item:center;">
+    <label for="">Godam</label>
             <select class="advance-select-box form-control" id="danaGroupId_model" name="godam_id" required>
                  <option value="" selected disabled>{{ __('Select Godam') }}</option>
+                 <option value="all">All Stocks</option>
                     @foreach ($godams as $godam)
                     <option value="{{ $godam->id }}">{{ $godam->department }}</option>
                     @endforeach
             </select>
-    <button type="submit" >Show Report</button>
-    </div>
+    <button class="btn btn-primary" style="width:200px" type="submit" >Show Report</button>
 </div>
 </form>
-<center>
-  <img class="lg-logo" src="{{ $settings->logo }}" alt="{{ $settings->companyName}}" width="350" height="80">
-</center>
 <div class="row">
     <div class="col-md-12">
         <div class="p-0 table-responsive table-custom my-3">
@@ -35,36 +44,29 @@
                 </thead>
 
                 <tbody>
-                @if ($autoloadStockDatas)
-                    @foreach ($autoloadStockDatas as $i => $stock)
-                        <tr>
-                            <td>{{ ++$i }}</td>
-                            <td>{{ $stock->plantType->name }}</td>
-                            <td>{{ $stock->plantName->name }}</td>
-                            <td>{{ $stock->danaName->name }}</td>
-                            <td>{{ $stock->shift->name }}</td>
-                            <td>{{ $stock->quantity}}</td>
-                        </tr>
-                    @endforeach
-                @else
-                     @foreach ($autoloadStocks as $i => $stock)
+                @if ($autoloadStocks)
+                    @foreach ($autoloadStocks as $i => $stock)
                     <tr>
                         <td>{{ ++$i }}</td>
-                        <td>{{ $stock->plantType->name }}</td>
-                        <td>{{ $stock->plantName->name }}</td>
-                        <td>{{ $stock->danaName->name }}</td>
-                        <td>{{ $stock->shift->name }}</td>
+                        <td>{{ $stock->plantTypeName }}</td>
+                        <td>{{ $stock->plantName}}</td>
+                        <td>{{ $stock->danaName}}</td>
+                        <td>{{ $stock->shiftName }}</td>
                         <td>{{ $stock->quantity}}</td>
                     </tr>
-                @endforeach
+                    @endforeach
                 @endif
-
-
-
                 </tbody>
-
             </table>
+
         </div>
+        @if ($autoloadStocks)
+            {{$autoloadStocks->links()}}
+        @endif
     </div>
 </div>
+@endsection
+@section('extra-script')
+  <script src="{{ asset('js/select2/select2.min.js') }}"></script>
+
 @endsection
