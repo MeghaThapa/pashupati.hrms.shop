8ccadeea713ee52754e11c8fb7630ae46c36a51d
bhabish1122 <bhabish1122@gmail.com>
2023-06-20 17:02:17 +0545

    bag entry transfer ....

---
+++ database/migrations/2023_06_20_074848_create_fabric_receive_for_bag_temporary.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bag_temporary_fabric_receive', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('fabric_id');
+            $table->foreign("fabric_id")->references("id")->on('fabrics')->onDelete("cascade");
+            $table->string('gram');
+            $table->string('gross_wt');
+            $table->string('net_wt');
+            $table->string('meter');
+            $table->string('roll_no');
+            $table->string('loom_no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fabric_receive_for_bag_temporary');
+    }
+};
