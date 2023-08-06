b5fa10c619c66e5c29b48cf5f84d5629523e5034
Megha thapa <meghathapaa@gmail.com>
2023-08-06 17:04:49 +0545

    bswFabric updates

--- app/Http/Controllers/BswLamFabForPrintingEntryController.php
+++ app/Http/Controllers/BswLamFabForPrintingEntryController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\BswLamFabForPrintingEntry;
+use App\Models\BswLamPrintedFabricStock;
+use App\Models\BswSentLamFab;
 use Illuminate\Http\Request;
+use DB;
 
 class BswLamFabForPrintingEntryController extends Controller
 {
@@ -12,20 +15,48 @@ class BswLamFabForPrintingEntryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function saveEntire(Request $request)
     {
-        //
+        try{
+        DB::beginTransaction();
+        $bswLamFabForPrintingEntry =BswLamFabForPrintingEntry::find($request->bswfabEntry_id);
+        $bswLamFabForPrintingEntry->status= 'completed';
+        $bswLamFabForPrintingEntry->save();
+
+        $bswLamPrintedFabricStocks= BswLamPrintedFabricStock::where('bswfabEntry_id',$request->bswfabEntry_id)->get();
+        if ($bswLamPrintedFabricStocks->isNotEmpty()) {
+        foreach ($bswLamPrintedFabricStocks as $stock) {
+            $stock->status = 'completed';
+            $stock->save();
+        }
+        }
+
+
+        $bswSentLamFabs=BswSentLamFab::where('bsw_lam_fab_for_printing_entry_id',$request->bswfabEntry_id)->get();
+        if ($bswSentLamFabs->isNotEmpty()) {
+        foreach ($bswSentLamFabs as $bswSentLamFab) {
+            $bswSentLamFab->status = 'completed';
+            $bswSentLamFab->save();
+        }
+        }
+        // return redirect()->route('BswLamFabSendForPrinting.index');
+        DB::commit();
+
+        }catch(Exception $ex){
+             $errorMessage = 'An error occurred: ' . $ex->getMessage();
+             return response()->json(['error' => $errorMessage]);
+            DB::rollback();
+        }
     }
+
+
 
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
--- app/Models/BswSentLamFab.php
+++ app/Models/BswSentLamFab.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class BswSentLamFab extends Model
 {
     use HasFactory;
+    protected $table = "bsw_sent_lam_fabs";
 
     public function fabric(){
         return $this->belongsTo(Fabric::class,'fabric_id','id');
