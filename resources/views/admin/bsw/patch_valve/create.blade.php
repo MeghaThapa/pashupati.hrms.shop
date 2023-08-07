6e7829e5c646c132dfcc43d0e7b0793edc38be7f
bhabish1122 <bhabish1122@gmail.com>
2023-08-07 16:00:44 +0545

    valave create blade

--- resources/views/admin/bsw/patch_valve/create.blade.php
+++ resources/views/admin/bsw/patch_valve/create.blade.php
@@ -78,38 +78,178 @@
    <div class="card">
     <div class="card-body p-0 m-0">
         <div class="row">
-            <form action="" method="post">
-                @csrf
-                <div class="col-md-3">
-                    <label for="">Receipt Number</label>
-                    <input type="text" value="" class="form-control" readonly/>
-                </div>
-                <div class="col-md-3">
-                    <label for="">Godam</label>
-                   <select name="godam" class="form-control" id="godam">
-                        <option value="">PSI</option>
-                   </select>
-                </div>
-                <div class="col-md-3">
-                    <label for="">Plant Type</label>
-                    <select name="plant_type" class="form-control" id="plant_type">
-                        <option value="">plant type</option>
-                   </select>
-                </div>
-                <div class="col-md-3">
-                    <label for="">Plant Name</label>
-                    <select name="plant_name" class="form-control" id="plant_name">
-                        <option value="">plant name</option>
-                   </select>
-                </div>
-                <div class="col-md-3">
-                    <label for="">Shift</label>
-                    <select name="shift" class="form-control" id="shift">
-                        <option value="">Morning</option>
-                   </select>
-                </div>
-            </form>
+            <div class="col-md-3">
+                <label for="">Receipt Number</label>
+                <input type="text" value="" class="form-control" readonly/>
+            </div>
+            <div class="col-md-3">
+                <label for="">Godam</label>
+                <select name="godam" class="form-control" id="godam">
+                    <option value="">PSI</option>
+                </select>
+            </div>
+            <div class="col-md-3">
+                <label for="">Plant Type</label>
+                <select name="plant_type" class="form-control" id="plant_type">
+                    <option value="">plant type</option>
+                </select>
+            </div>
+            <div class="col-md-3">
+                <label for="">Plant Name</label>
+                <select name="plant_name" class="form-control" id="plant_name">
+                    <option value="">plant name</option>
+                </select>
+            </div>
+            <div class="col-md-3">
+                <label for="">Shift</label>
+                <select name="shift" class="form-control" id="shift">
+                    <option value="">Morning</option>
+                </select>
+            </div>
+            <div class="col-md-3">
+                <label for="">Fabric Type</label>
+                <select name="fabric_type" class="form-control" id="fabric_type">
+                    <option value="">LAM</option>
+                    <option value="">Unlam</option>
+                </select>
+            </div>
+            <div class="col-md-3">
+                <label for="">Fabric</label>
+                <select name="fabric" id="fabric">
+                    <option value="">Fabric 1</option>
+                </select>
+            </div>
         </div>
+
+        <div class="table-responsive card">
+            <div class="card-header">
+                <h3>Fabric List</h3>
+            </div>
+            <div class="card-body">
+                <table class="table table-hover table-striped w-100">
+                    <thead>
+                         <tr>
+                             <th>Fabric Name</th>
+                             <th>Roll No</th>
+                             <th>GW</th>
+                             <th>NW</th>
+                             <th>Meter</th>
+                         </tr>
+                    </thead>
+                    <tbody class="fabnic-list">
+                    </tbody>
+                 </table>
+            </div>
+        </div>
+
+        <div class="table-responsive card">
+            <div class="card-header">
+                <h3>Issued Fabric</h3>
+            </div>
+            <div class="card-body">
+                <table class="table table-hover table-striped w-100">
+                    <thead>
+                         <tr>
+                             <th>Fabric Name</th>
+                             <th>Roll No</th>
+                             <th>GW</th>
+                             <th>NW</th>
+                             <th>Meter</th>
+                             <th>Action</th>
+                         </tr>
+                    </thead>
+                    <tbody class="fabnic-list">
+                    </tbody>
+                 </table>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="table-responsive card">
+            <div class="card-header">
+                <h3>Sent Bag</h3>
+            </div>
+            <div class="card-body">
+                <table class="table table-hover table-striped w-100">
+                    <thead>
+                         <tr>
+                             <th>Fabric Name</th>
+                             <th>Roll No</th>
+                             <th>GW</th>
+                             <th>NW</th>
+                             <th>Meter</th>
+                             <th>Avg</th>
+                             <th>Type</th>
+                             <th>Bag Brand</th>
+                             <th>Brand Category</th>
+                             <th>Action</th>
+                         </tr>
+                    </thead>
+                    <tbody class="fabnic-list">
+                    </tbody>
+                 </table>
+            </div>
+        </div>
+
+        <div class="form">
+           <form action="" method="post">
+            @csrf
+            <div class="row">
+                <div class="col-md-3">
+                    <label for="">Total Out Meter</label>
+                    <input type="text" name="total_out_meter" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="">Total Valve Meter</label>
+                    <input type="text" name="total_out_meter" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="">Total Patch Meter</label>
+                    <input type="text" name="total_out_meter" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="">Total In Meter</label>
+                    <input type="text" name="total_in_meter" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="">Diff In/Out Meter</label>
+                    <input type="text" name="diff_in_out_meter" class="form-control">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-3">
+                    <label for="">Total Out NW</label>
+                    <input type="text" name="total_out_nw" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="">Total Valve NW</label>
+                    <input type="text" name="total_out_nw" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="">Total Patch NW</label>
+                    <input type="text" name="total_out_nw" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="">Total In NW</label>
+                    <input type="text" name="total_in_nw" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="">Diff In/Out NW</label>
+                    <input type="text" name="diff_in_out_nw" class="form-control">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <label for=""></label>
+                </div>
+            </div>
+
+           </form>
+        </div>
+
     </div>
    </div>
 @endsection
